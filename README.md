edab90c2dddd3010ae5dded471851c064f916bc7
Marco Meschini <mmeschini01@gmail.com>
2023-10-24 10:00:47 +0000

    First implementation

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Research Track 1: first assignment
+This is the script for the first assignment of the course Research Track 1 at UniGE.
+
+The goal is to let the robot bring all the tokens in the same position.
+
+In order to run the script, go inside the folder and write:
+
+```bash
+$ python run.py exercise1.py
+```
+
+```python
+#Set global variables
+ a_th = 2.0 #threshold to control linear distance
+ d_th = 0.4 #threshold to control orientation
+grabbed = False #used to know if a token is grabbed by the robot
+
+R = Robot() #instance an object of class robot
+
+function drive(speed, seconds) #This function set a linear velocity to the robot with certain speed and for x seconds
+
+function turn(speed, seconds) This function is used to set an angular velocity to the robot
+
+function select_nearest_token(token_id) #Let the robot choose the nearest token in is sight not already taken before
+
+function go_to_token(selected_token) #This function is used to search the token selected by the previous function and to bring the robot to it.
+
+Define the main function:
+
+Create a list of token_id and put the code of the first token in it.
+Create a counter to store the information of the number of tokens already taken
+
+While counter is less than 6:
+
+	Selected_token = select_nearest_token(token_id)
+	
+	If selected_token not found:
+		Turn(speed, seconds)
+		
+	Else:
+	
+		Go_to_token(selected_token)
+		Token_id.append(selected_token) #add to the list the token id taken 
+		go_to_token(token_id[0]) #go to the first saved token
+		counter incrementation
+```
--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -3,8 +3,127 @@ from __future__ import print_function
 import time
 from sr.robot import *
 
+a_th = 2.0
+""" float: Threshold for the control of the linear distance"""
+
+d_th = 0.4
+""" float: Threshold for the control of the orientation"""
+
+grabbed = False #Boolean to know if the robot is grabbing a token
+
 R = Robot()
+""" instance of the class Robot"""
 
 
-while 1:
-    pass
+def drive(speed, seconds):
+    """
+    Function for setting a linear velocity
+    
+    Args: speed (int): the speed of the wheels
+	  seconds (int): the time interval
+    """
+    R.motors[0].m0.power = speed
+    R.motors[0].m1.power = speed
+    time.sleep(seconds)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+
+def turn(speed, seconds):
+    """
+    Function for setting an angular velocity
+    
+    Args: speed (int): the speed of the wheels
+	  seconds (int): the time interval
+    """
+    R.motors[0].m0.power = speed
+    R.motors[0].m1.power = -speed
+    time.sleep(seconds)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+   	
+   	
+   	
+def select_nearest_token(token_id):
+    """
+    Function to find the nearest token not already taken before
+    
+    Arg: token_id (list): list of token already brought in position
+    """
+    dist=100
+    
+    for token in R.see():   
+
+        if token.dist < dist and token.info.code not in token_id:
+            dist=token.dist
+	    selected_token = token.info.code
+    if dist==100:
+	return -1
+    else:
+   	return selected_token
+
+
+
+	
+def go_to_token(selected_token):
+
+	"""
+	Function used to find the token selected by select_nearest_token and to bring
+	the robot to it.
+	
+	Arg: selected_token(int): code of the selected token
+	"""
+	global grabbed
+	found = False
+	while 1:
+	
+		for token in R.see():
+			if token.info.code == selected_token:
+				dist=token.dist
+				rot_y=token.rot_y
+				found = True
+				
+	 	if found == True and dist < 2*d_th and grabbed is True:
+			grabbed = False
+			R.release()
+			break
+			
+		elif found == True and dist > d_th:
+			if -a_th<= rot_y <= a_th: # if the robot is well aligned with the token, we go forward
+				print("Ah, that'll do.")
+				drive(50, 0.5)
+			elif rot_y < -a_th: # if the robot is not well aligned with the token, we move it on the left or on the right
+				print("Left a bit...")
+			   	turn(-3, 0.5)
+			elif rot_y > a_th:
+				print("Right a bit...")
+				turn(+3, 0.5)
+				
+		elif found == True and dist < d_th and grabbed is False:
+			grabbed = True
+			R.grab()
+			break
+			
+
+		else:
+			turn(20,0.5)
+			    	
+		    	
+
+def main():
+
+	token_id = [-1] #Create the list to store the token id
+	token_id[0] = R.see()[0].info.code #saving the code of the first token where all the other will be brought
+	counter = 1
+	
+	while counter < 6:
+		selected_token = select_nearest_token(token_id)
+		
+		if selected_token == -1:
+			turn(20,0.5)
+		else:
+			go_to_token(selected_token)
+			token_id.append(selected_token)
+			go_to_token(token_id[0])
+			counter = counter + 1
+			
+main()
