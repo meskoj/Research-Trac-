6abdeafb08bbebeb9f5799f95de8a88007b97ffe
Marco Meschini <mmeschini01@gmail.com>
2023-11-11 11:04:29 +0000

    Added more robust function and comments

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-
 import time
 from sr.robot import *
 
@@ -7,13 +6,13 @@ a_th = 2.0
 #float: Threshold for the control of the orientation
 
 d_th = 0.4
-# float: Threshold for the control of the linear distance
+#float: Threshold for the control of the linear distance
 
 grabbed = False
-#Boolean to know if the robot has grabbed a token
+#Boolean: To know if the robot has grabbed a token
 
 R = Robot()
-""" instance of the class Robot"""
+#Instance of the class Robot
 
 
 def drive(speed, seconds):
@@ -50,14 +49,14 @@ def select_nearest_token(token_id):
     
     Arg: token_id (list): list of tokens already brought in position
     """
-    dist=100
+    dist = 100
     
     for token in R.see():   
-
         if token.dist < dist and token.info.code not in token_id:
-            dist=token.dist
+            dist = token.dist
 	    selected_token = token.info.code
-    if dist==100:
+	    
+    if dist == 100:
 	return -1
     else:
    	return selected_token
@@ -66,10 +65,9 @@ def select_nearest_token(token_id):
 
 	
 def go_to_token(selected_token):
-
 	"""
-	Function used to find the token selected by select_nearest_token and to bring
-	the robot to it.
+	Function used to find the token selected by select_nearest_token and let the robot
+	go to it
 	
 	Arg: selected_token(int): code of the selected token
 	"""
@@ -79,33 +77,34 @@ def go_to_token(selected_token):
 	
 		for token in R.see():
 			if token.info.code == selected_token:
-				dist=token.dist
-				rot_y=token.rot_y
+				dist = token.dist
+				rot_y = token.rot_y
 				found = True
 				
-	 	if found == True and dist < 2*d_th and grabbed is True:
+	 	if found == True and dist < 2*d_th and grabbed is True: #if the robot is near the token selected to group all the tokens, 
+	 								#it release the token in the hand
 			grabbed = False
 			R.release()
 			drive(-50, 0.5)
 			break
 			
-		elif found == True and dist > d_th:
-			if -a_th<= rot_y <= a_th: # if the robot is well aligned with the token, we go forward
+		elif found == True and dist > d_th: #if the robot is far from the token, the robot goes towards it
+			if -a_th <= rot_y <= a_th: #if the robot is well aligned with the token, it goes forward
 				print("Ah, that'll do.")
 				drive(50, 0.5)
-			elif rot_y < -a_th: # if the robot is not well aligned with the token, we move it on the left or on the right
+			elif rot_y < -a_th: #if the robot is not well aligned with the token, it moves on the left or on the right
 				print("Left a bit...")
-			   	turn(-3, 0.5)
+			   	turn(-2, 0.5)
 			elif rot_y > a_th:
 				print("Right a bit...")
-				turn(+3, 0.5)
+				turn(+2, 0.5)
 				
-		elif found == True and dist < d_th and grabbed is False:
+		elif found == True and dist < d_th and grabbed is False: #if the robot is near the token to grab, the robot grabs it
 			grabbed = True
 			R.grab()
 			break
 			
-		else:
+		else: #if the token has not been found, the robot turns
 			turn(20,0.5)
 
 
@@ -115,9 +114,8 @@ def count_token():
 	"""
 	
 	token_list = []
-	for i in range(10):
+	for i in range(10): #the robot revolves around itself to find all the tokens
 		for token in R.see():
-			print(token.info.code)
 			if token.info.code not in token_list:
 				token_list.append(token.info.code)
 		turn(25,0.5)
@@ -125,12 +123,17 @@ def count_token():
 
 def main():
 
-	counter = count_token()
-	token_id = [-1] #Create the list to store the token id
-	token_id[0] = R.see()[0].info.code #Saving the code of the first token where all the other will be brought
-	i = 1
+	token_number = count_token()
+	token_id = [] #Create the list to store the token id
 	
-	while i < counter:
+	while len(R.see()) == 0 : #Until the token is not found, turn
+		turn(25,0.5) 
+		
+	first_token = R.see()[0].info.code #Saving the code of the first token where all the others will be brought		
+	token_id.append(first_token)
+	
+	i = 1
+	while i < token_number:
 		selected_token = select_nearest_token(token_id)
 		
 		if selected_token == -1:
